dad034ee5cf2fc54b851843a3d23ec499ac63845
Shiva Rapol <97930299+ShivaRap@users.noreply.github.com>
2023-01-24 14:31:29 +0530

    added newDeploy()

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,5 +2,8 @@ def gitDown(repo) {
   git "${repo}"
 }
 def buildArt{
-   sh '''mvn package'''
+   sh "mvn package"
+}
+def newDeploy(workspace, ip, contextPath){
+	sh "scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextPath}.war"
 }
