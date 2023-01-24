ae7b82c87934dd3f3c02cefd5b96fe0c296c30b6
Shiva Rapol <97930299+ShivaRap@users.noreply.github.com>
2023-01-24 15:37:04 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,3 +7,6 @@ def mavenBuild(){
 def deploy(workspace,ip,contextPath){
   sh "scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextPath}.war"
 }
+def runSelenium(workspace){
+  sh "java -jar /var/lib/jenkins/workspace/${workspace}/testing.jar"
+}
