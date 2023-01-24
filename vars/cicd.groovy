23efc43dc037dde48c26f88b547f4161e44ad043
Shiva Rapol <97930299+ShivaRap@users.noreply.github.com>
2023-01-24 15:01:24 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,9 +2,7 @@ def gitDown(repo) {
   git "${repo}"
 }
 def mavenBuild{
-   sh "mvn package"
+   sh '''mvn package'''
 }
-def newDeploy(workspace, ip, contextPath){
-   sh "scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextPath}.war"
-}
+
  
